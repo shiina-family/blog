8d5aefd26c34f9da2c16f3bee51933d096fabafd
Tenzyu Masuda <tenzyu.on@gmail.com>
2021-08-25 23:01:40 +0900

    test

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+const IndexPage = () => {
+  return (
+    <main>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, quidem?</p>
+    </main>
+  )
+}
+
+export default IndexPage
