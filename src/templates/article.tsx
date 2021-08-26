85e9d3de852496dea553ef753a11a7cf83b973f8
Tenzyu Masuda <tenzyu.on@gmail.com>
2021-08-27 01:27:50 +0900

    put article page template

---
+++ src/templates/article.tsx
@@ -0,0 +1,53 @@
+import React from "react"
+import { graphql } from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+
+interface ArticleType {
+  data: {
+    mdx: {
+      html: string,
+      frontmatter: {
+        date: string,
+        thumbnail: string,
+        title: string,
+        writer: string
+      }
+      body: string,
+      slug: string
+    }
+  }
+}
+
+const Article: React.FC<ArticleType> = ({ data }) => {
+  return (
+    <div>
+      <article>
+        {/* Todo: 画像に置き換える */}
+        <div>{data.mdx.frontmatter.thumbnail}</div>
+        <h1>{data.mdx.frontmatter.title}</h1>
+        <div>
+          <span>{data.mdx.frontmatter.writer}</span>
+          <span>{data.mdx.frontmatter.date}</span>
+        </div>
+        <MDXRenderer>{data.mdx.body}</MDXRenderer>
+      </article>
+    </div>
+  )
+}
+
+export default Article;
+
+export const query = graphql`
+    query articleTemplate($id: String!) {
+      mdx(id: {eq: $id}) {
+        frontmatter {
+          date(formatString: "YYYY年MM月DD日")
+          thumbnail
+          title
+          writer
+        }
+        body
+        slug
+      }
+    }
+`
