2128bff476b5535f8f187b741d7ac3637045c1e1
Tenzyu Masuda <tenzyu.on@gmail.com>
2021-08-27 01:27:38 +0900

    use article list component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
-import * as React from "react"
-
+import * as React from "react";
+import ArticleList  from "../components/article-list"
 const IndexPage = () => {
   return (
     <main>
+        <ArticleList></ArticleList>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, quidem?</p>
     </main>
   )
