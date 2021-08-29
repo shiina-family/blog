a9eae6757ff953d742965185fe5983dffc0c66e5
Tenzyu Masuda <tenzyu.on@gmail.com>
2021-08-30 05:04:07 +0900

    create header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Link}from 'gatsby';
-
+import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const Header: React.FC = () => {
   return (
-    <header className="h-16 bg-blue-500">
+    <header className="h-16 bg-black flex items-center fixed top-0 w-full z-50 px-4">
       <Link to="/">
-      <div>Shiina</div>
+        <StaticImage src="../../../content/assets/icon.png" alt="logo" width={44} />
       </Link>
     </header>
   )
