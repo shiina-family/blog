82c8110c849d07fde05e1e8ca4ed7077b4664704
Tenzyu Masuda <tenzyu.on@gmail.com>
2021-08-29 21:15:33 +0900

    put footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="h-16 bg-purple-500">
+      <small>(c) Tenzyu Masuda</small>
+    </footer>
+  )
+}
+
+export default Footer;
