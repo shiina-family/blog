a38343be5c194a561dc7923857344abb82ab6e05
Tenzyu Masuda <tenzyu.on@gmail.com>
2021-08-30 05:04:25 +0900

    improve article templates

--- src/templates/article.tsx
+++ src/templates/article.tsx
@@ -3,6 +3,7 @@ import { graphql, Link } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import { GatsbyImage, getImage, ImageDataLike } from "gatsby-plugin-image"
 import Layout from "../layouts/article";
+import { getAvatarOf } from "../components/avatar";
 
 interface ArticleType {
   data: {
@@ -24,18 +25,23 @@ interface ArticleType {
 const Article: React.FC<ArticleType> = ({ data }) => {
   return (
     <Layout>
-      <article className="prose mx-auto px-4">
-        <h1 className="pt-8">{data.mdx.frontmatter.title}</h1>
-        {/* GatsbyImage: gatsby-image-wrapper あたりの影響で、 */}
-        {/* dev段階では描画がズレる。ビルド後は問題ない。 */}
-        <GatsbyImage image={getImage(data.mdx.frontmatter.thumbnail)!} alt="a" />
-        <div>
-          <span>{data.mdx.frontmatter.writer}</span>
-          <span>{data.mdx.frontmatter.date}</span>
+      <article className="container max-w-screen-blog mx-auto px-4">
+        <h1 className="py-8 text-4xl font-bold">{data.mdx.frontmatter.title}</h1>
+        <GatsbyImage className="rounded-md z-10" image={getImage(data.mdx.frontmatter.thumbnail)!} alt="a" />
+        <div className="border-t border-b border-gray-700 py-1 my-4 px-0 flex items-center">
+          <GatsbyImage className="mx-2 w-12 h-12 rounded-full z-10" image={getImage(getAvatarOf(data.mdx.frontmatter.writer))!} alt="icon of writer" />
+          <div>
+            <p className="text-lg font-semibold">{data.mdx.frontmatter.writer}</p>
+            <time className="text-sm text-gray-400">{data.mdx.frontmatter.date} に公開</time>
+          </div>
         </div>
-        <MDXRenderer>{data.mdx.body}</MDXRenderer>
-        <Link to="/">back</Link>
+
+        <div className="prose">
+          <MDXRenderer>{data.mdx.body}</MDXRenderer>
+        </div>
+
       </article>
+      <Link to="/" className="block font-bold text-2xl py-4 text-center text-blue-500">back</Link>
     </Layout>
   )
 }
