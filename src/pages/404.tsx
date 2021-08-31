f07de830e6c3c811a07a4077abaf13365da878ac
Tenzyu Masuda <tenzyu.on@gmail.com>
2021-08-31 12:10:19 +0900

    unuse Layout component

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,16 +1,13 @@
 import { Link } from 'gatsby';
 import React from 'react';
-import Layout from '../layouts/single';
 
 const NotFoundPage = () => (
-  <Layout>
-    <article>
-      <h1>404: Page not found.</h1>
-      <p>
-        You&apos;ve hit the void. <Link to="/">Go back.</Link>
-      </p>
-    </article>
-  </Layout>
+  <article>
+    <h1>404: Page not found.</h1>
+    <p>
+      You&apos;ve hit the void. <Link to="/">Go back.</Link>
+    </p>
+  </article>
 );
 
 export default NotFoundPage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
 import * as React from "react";
 import ArticleList from "../components/article-list"
-import Layout from "../layouts/single";
 
 const IndexPage = () => {
   return (
-    <Layout>
-      <div className="container mx-auto px-4 max-w-xl min-h-full">
-        <ArticleList />
-      </div>
-    </Layout>
+    <div className="container mx-auto px-4 max-w-xl min-h-full">
+      <ArticleList />
+    </div>
   )
 }
 
